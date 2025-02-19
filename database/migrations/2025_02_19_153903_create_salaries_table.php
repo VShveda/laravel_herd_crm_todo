232d1a79a0d8fcb89d7d5ff8b5bbe20e2c9b815e
viktor <vinix.ru@gmail.com>
2025-02-19 17:46:30 +0200

    add salaries  table

---
+++ database/migrations/2025_02_19_153903_create_salaries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')
+                ->references('id')
+                ->on('employees');
+            $table->decimal('amount', 10, 2);
+            $table->date('payment_date');
+            $table->boolean('is_paid')
+                ->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
